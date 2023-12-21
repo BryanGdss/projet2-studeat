0919c81076535de6cdcf3753b99a13b7620890c2
BryanGdss <bryangil@hotmail.be>
2023-12-21 13:47:07 +0100

    commit fixs

--- templates/template-commande.php
+++ templates/template-commande.php
@@ -4,10 +4,10 @@
 ?>
 <?php get_header(); ?>
 <div class="menuchoix">
-            <div class="row row-cols-5 g-3"></div>
+            <div class="row row-cols-1 row-cols-md-5  g-3"></div>
              <div class="col">
                 <div class="card-custom">
-                 <a href="<?php echo "restaurant" ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/restaurant.png" class="d-block w-100" class="card-img-top" alt="L'image ne s'affiche pas" />
+                 <a href="<?php echo "restaurant" ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/restaurant.png" class="d-block card-img-top img-fluid"  alt="L'image ne s'affiche pas" />
                   <div class="card-body">
                     <h5 class="card-title">Restaurants</h5>
                   </a>
@@ -16,7 +16,7 @@
               </div>
               <div class="col">
                 <div class="card-custom">
-                  <a href="<?php echo "vegetarien" ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/vegetarien.png" class="d-block w-100" class="card-img-top" alt="L'image ne s'affiche pas" />
+                  <a href="<?php echo "vegetarien" ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/vegetarien.png" class="d-block card-img-top img-fluid"  alt="L'image ne s'affiche pas" />
                   <div class="card-body">
                     <h5 class="card-title">Végétariens</h5>
                   </a>
@@ -25,7 +25,7 @@
               </div>
               <div class="col">
                 <div class="card-custom">
-                  <a href="<?php echo "halal" ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/halal.png" class="d-block w-100" class="card-img-top" alt="L'image ne s'affiche pas" /> 
+                  <a href="<?php echo "halal" ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/halal.png" class="d-block card-img-top img-fluid" alt="L'image ne s'affiche pas" /> 
                   <div class="card-body">
                     <h5 class="card-title">Halal</h5>
                   </a>
@@ -34,7 +34,7 @@
               </div>
               <div class="col">
                 <div class="card-custom ">
-                <a href="fast-food"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/fastfood.png" class="d-block w-100" class="card-img-top" alt="L'image ne s'affiche pas" />
+                <a href="fast-food"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/fastfood.png" class="d-block card-img-top img-fluid" alt="L'image ne s'affiche pas" />
                   <div class="card-body">
                     <h5 class="card-title">Fast-foods</h5>
                   </a>
@@ -44,7 +44,7 @@
               
               <div class="col">
                 <div class="card-custom" >
-                 <a href="courses"> <img src="<?php echo get_template_directory_uri(); ?>/assets/img/courses.png" class="d-block w-100" class="card-img-top " alt="L'image ne s'affiche pas" />
+                 <a href="courses"> <img src="<?php echo get_template_directory_uri(); ?>/assets/img/courses.png" class="d-block card-img-top img-fluid" alt="L'image ne s'affiche pas" />
                   <div class="card-body">
                     <h5 class="card-title">Courses</h5>
                   </a>
