b64b4f9d13bb52820f06d19d351276683f680844
BryanGdss <bryangil@hotmail.be>
2023-12-21 11:22:39 +0100

    fix de l'erreur de mes forms

--- templates/template-contact.php
+++ templates/template-contact.php
@@ -4,114 +4,21 @@
 ?>
 <?php get_header(); ?>
 
-<style> 
-.contactez-nous {
-    width: 80%;
-    max-width: 700px;
-    border: 1px solid;
-    border-radius: 8px;
-    padding: 0 20px;
-    background: #449653;
-    margin-bottom: 7%;
-  }
 
-  @media (max-width: 768px) {
-    .contactez-nous {
-      width: 90%;
-      padding: 0 10px; 
-    }
-  }
-    
-    .contactez-nous > h1 {
-    font-weight: 500;
-    }
-    
-    .contactez-nous > p {
-    font-weight: 300;
-    }
-    
-    form div {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    min-height: 83px;
-    margin-top: 25px;
-    }
-    
-    form div > label {
-    margin-bottom: 7px;
-    font-weight: 600;
-    }
-    
-    form div > input, form div > select, form div > textarea {
-    background-color: #eee  ;
-    border: none;
-    border-radius: 5px;
-    font-size: 16px;
-    font-weight: 500;
-    box-shadow: 0px 2px 2px 0px rgba(0,0,0,0.25);
-    
-    }
-    
-    form div > input, form div > select {
-    height: 50px;
-    padding-left: 10px;
-    }
-    
-    form div > select {
-    appearance: none;
-    background-size: 15px;
-    background-position: right 10px top 50%;
-  
-    }
-    
-    form div > textarea {
-    height: 195px;
-    padding: 15px 0px 0px 10px;
-    }
-    
-    form div > input::placeholder, form div > textarea::placeholder {
-    color: #606361;
-    }
-    
-    form div > select:invalid {
-    color: #606361;
-    }
-    
-    form div > select option {
-    background: #606361;
-    color: #606361;
-    }
-    
-    form div:last-child {
-    align-items:center;
-    margin-top: 20px;
-    }
-    
-    form button {
-    
-    font-weight: 700;
-    font-size: 28px;
-    background: #eee;
-    border: rgba(48, 48, 48, 0.5) solid 1px;
-    border-radius: 5px;
-    box-shadow: 0px 4px 4px 0px rgba(0,0,0,0.25);
-    color: #303030;
-    }
 </style>
 <div class="contactez-nous">
 <h1>Contactez-nous</h1>
 <p>Un problème, une question, envie de nous envoyer un message d’amour ? N’hésitez pas à utiliser ce formulaire pour prendre contact avec nous !</p>
 <form action="/page-traitement-donnees" method="post">
-<div>
+<div class="cadre">
 <label for="nom">Votre nom</label>
 <input type="text" id="nom" name="nom" placeholder="Martin" required>
 </div>
-<div>
+<div class="cadre">
 <label for="email">Votre e-mail</label>
 <input type="email" id="email" name="email" placeholder="monadresse@mail.com" required>
 </div>
-<div>
+<div class="cadre">
 <label for="sujet">Quel est le sujet de votre message ?</label>
 <select name="sujet" id="sujet" required>
 <option value="" disabled selected hidden>Choisissez le sujet de votre message</option>
@@ -121,7 +28,7 @@
 <option value="autre">Autre...</option>
 </select>
 </div>
-<div>
+<div class="cadre">
 <label for="message">Votre message</label>
 <textarea id="message" name="message" placeholder="Bonjour, je vous contacte car...." required></textarea>
 </div>
